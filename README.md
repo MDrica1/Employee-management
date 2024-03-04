79f3d8cee4f49d5eaac795158ca3147187e96658
Andriele Mendonça <dielemendonca@hotmail.com>
2024-03-04 16:24:39 -0300

    -- Final Version

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Java-POO-projects: Employee-management
+# Java-OOP-projects: Employee-management
 Project developed for OxeTech Lab's Java OOP course.
 
 The classes, attributes and methods that were used in this project are as follows:
 
-![diagram](image/diagram.png "UML Class Diagram")
+![diagram](image/diagram.jpg "UML Class Diagram")
 
--- src/App.java
+++ src/App.java
@@ -55,12 +55,12 @@ public class App {
                 if (funcionario.getMatricula().equals(matricula)) {
                     matriculaExistente = true;
                     System.out.println("Este nº de matrícula já consta no sistema. Por favor, tente novamente.");
-                    break; // Se a matrícula já existe, não é necessário continuar verificando
+                    break; 
                 }
             }
 
             if (!matriculaExistente) {
-                matriculaNova = true; // Se a matrícula não existe, sai do loop
+                matriculaNova = true; 
             }
         }
 
@@ -84,160 +84,288 @@ public class App {
         Funcionario F1 = new Funcionario(nome, cpf, matricula, departamento, salario);
         funcionarios.add(F1);
     }
-        public static void remover (Scanner novoFuncionario){
+    public static void remover (Scanner removeFuncionario){
 
 
-            if (funcionarios.isEmpty()) {
-                System.out.println("Não há funcionários a remover.");
-            } else {
-                System.out.println("Qual o número de MATRÍCULA do funcionário que deseja remover?");
-                String funcionarioRemover = novoFuncionario.nextLine();
-                boolean funcionarioEncontrado = false;
+        if (funcionarios.isEmpty()) {
+            System.out.println("Não há funcionários a remover.");
+        } else {
 
+            boolean funcionarioEncontrado = false;
+            while (!funcionarioEncontrado){
                 ArrayList<Funcionario> copiaFuncionarios = new ArrayList<>(funcionarios);
-
+                System.out.println("Qual o número de MATRÍCULA do funcionário que deseja remover?");
+                String funcionarioRemover = removeFuncionario.nextLine();
                 for (Funcionario funcionario : copiaFuncionarios) {
                     if (funcionario.getMatricula().equals(funcionarioRemover)) {
                         funcionarios.remove(funcionario);
                         funcionarioEncontrado = true;
                         System.out.println("Funcionário " + funcionario.getNome() + " removido com sucesso!");
+                        break;
                     }
                 }
                 if (!funcionarioEncontrado) {
                     System.out.println("Funcionário não encontrado.");
+                    }
                 }
-            }
+        }
 
+    }
+
+
+    public static void consultar (ArrayList < Funcionario > funcionarios) {
+
+        for (Funcionario funcionario : funcionarios) {
+            departamentos.add(funcionario.departamento);
         }
 
 
-        public static void consultar (ArrayList < Funcionario > funcionarios) {
+        for (String departamento : departamentos) {
+
+            int cont = 0;
+            ArrayList<Funcionario> f = new ArrayList<>();
 
             for (Funcionario funcionario : funcionarios) {
-                departamentos.add(funcionario.departamento);
-            }
-
-
-            for (String departamento : departamentos) {
-
-                int cont = 0;
-                ArrayList<Funcionario> f = new ArrayList<>();
-
-                for (Funcionario funcionario : funcionarios) {
-                    if (funcionario.departamento.equals(departamento)) {
-                        f.add(funcionario);
-                        cont += 1;
-                    }
-
-                }
-                if (cont > 0) {
-                    System.out.println("Departamento: " + departamento + "\nNº de funcionários: " + cont + "\n");
-                    for (Funcionario funcionario : f) {
-                        System.out.println("Nome: " + funcionario.nome + "\nCPF: " + funcionario.cpf + "\nMatrícula: " + funcionario.matricula + "\nSalario: R$ " + funcionario.salario + "\n");
-                    }
+                if (funcionario.departamento.equals(departamento)) {
+                    f.add(funcionario);
+                    cont += 1;
                 }
 
             }
+            if (cont > 0) {
+                System.out.println("Departamento: " + departamento + "\nNº de funcionários: " + cont + "\n");
+                for (Funcionario funcionario : f) {
+                    System.out.println("Nome: " + funcionario.nome + "\nCPF: " + funcionario.cpf + "\nMatrícula: " + funcionario.matricula + "\nSalario: R$ " + funcionario.salario + "\n");
+                }
+            }
+
         }
+    }
 
-        public static void consultaSalario (Set < String > departamentos) {
+    public static void consultaSalario (Set < String > departamentos) {
+        for (Funcionario funcionario : funcionarios) {
+            departamentos.add(funcionario.departamento);
+        }
+        for (String departamento : departamentos) {
+            double salario = 0.0;
+            int n = 0;
+            ArrayList<Funcionario> f1 = new ArrayList<>();
+
+
             for (Funcionario funcionario : funcionarios) {
-                departamentos.add(funcionario.departamento);
+                if (funcionario.departamento.equals(departamento)) {
+                    n += 1;
+                    salario += funcionario.getSalario();
+                    f1.add(funcionario);
+
+
+                }
             }
-            for (String departamento : departamentos) {
-                double salario = 0.0;
-                int n = 0;
-                ArrayList<Funcionario> f1 = new ArrayList<>();
-
-
-                for (Funcionario funcionario : funcionarios) {
-                    if (funcionario.departamento.equals(departamento)) {
-                        n += 1;
-                        salario += funcionario.getSalario();
-                        f1.add(funcionario);
-
-
-                    }
+            if (n > 0) {
+                System.out.println("Departamento: " + departamento + "\nNº de funcionários: " + n + "\n");
+                for (Funcionario funcionario : f1) {
+                    System.out.println(funcionario.nome + ": R$ " + funcionario.salario);
                 }
-                if (n > 0) {
-                    System.out.println("Departamento: " + departamento + "\nNº de funcionários: " + n + "\n");
-                    for (Funcionario funcionario : f1) {
-                        System.out.println(funcionario.nome + ": R$ " + funcionario.salario);
-                    }
-                    System.out.println("\nSalário total: R$ " + salario + "\n");
+                System.out.println("\nSalário total: R$ " + salario + "\n");
+            }
+
+        }
+    }
+
+    public static void atualizaSalario(Scanner atualizaSalario){
+
+        boolean haMatricula = false;
+        while(!haMatricula) {
+            System.out.println("Matrícula do funcionário: ");
+            String matriculaF = atualizaSalario.nextLine();
+            for (Funcionario funcionario : funcionarios) {
+                if (funcionario.matricula.equals(matriculaF)) {
+                    System.out.println("Novo salário: ");
+                    double novoSalario = atualizaSalario.nextDouble();
+                    atualizaSalario.nextLine();
+                    System.out.println("Salário de " + funcionario.nome + " atualizado de R$ " + funcionario.salario + " para R$ " + novoSalario);
+                    funcionario.salario = novoSalario;
+                    haMatricula = true;
+                    break;
+
                 }
+            }
+            if (!haMatricula){
+                System.out.println("Funcionário não encontrada");
 
             }
         }
+    }
 
-        public static void main (String[]args){
+    public static void Realocar(Scanner realocaFuncionario){
 
-            Scanner sc = new Scanner(System.in);
-            String s2;
-            int n2;
-            do {
-                s2 = """
+        boolean matriculaEncontrada = false;
+        while (!matriculaEncontrada) {
+            System.out.println("Matrícula do funcionário: ");
+            String matriculaF2 = realocaFuncionario.nextLine();
+            for (Funcionario funcionario : funcionarios) {
+                if (funcionario.matricula.equals(matriculaF2)) {
+                    System.out.println("Novo departamento: ");
+                    String novoDepartamento = realocaFuncionario.nextLine();
+                    //realocaFuncionario.nextLine();
+                    System.out.println("O funcionário " + funcionario.nome + " foi transferido do departamento de " + funcionario.departamento + " para o de " + novoDepartamento);
+                    funcionario.departamento = novoDepartamento;
+                    matriculaEncontrada = true;
+                    break;
+
+                }
+            }
+            if (!matriculaEncontrada) {
+                System.out.println("Funcionário não encontrado.");
+
+            }
+        }
+    }
+    
+
+    public static void main (String[]args){
+
+        Scanner sc = new Scanner(System.in);
+        String s2;
+        int n2;
+        do {
+            s2 = """
                         O que deseja fazer? (Digite o NÚMERO):
                         1 - Adicionar funcionários
                         2 - Consultar funcionários
                         3 - Consultar salários por departamento
-                        4 - Remover funcionários
-                        5 - Sair da aplicação""";
-                System.out.println(s2);
-                n2 = sc.nextInt();
-                sc.nextLine();
+                        4 - Atualizar salários 
+                        5 - Realocar funcionários
+                        6 - Remover funcionários
+                        7 - Sair da aplicação""";
+            System.out.println(s2);
+            n2 = sc.nextInt();
+            sc.nextLine();
 
-                switch (n2) {
-                    case 1:
+            switch (n2) {
+                case 1:
 
-                        while (true) {
+                    while (true) {
 
-                            cadastrarFuncionario(sc);
+                        cadastrarFuncionario(sc);
 
-                            System.out.print("Deseja adicionar outro funcionário? (s/n): ");
-                            String resposta = sc.next();
-                            //novaTarefa.nextLine();
+                        System.out.print("Deseja adicionar outro funcionário? (s/n): ");
+                        String resposta = sc.next();
+                        //novaTarefa.nextLine();
 
-                            if (!resposta.equalsIgnoreCase("s")) {
-                                break;
-                            }
-
-                            sc.nextLine();
-                        }
-
-                        break;
-                    case 2:
-
-                        if (funcionarios.isEmpty()) {
-                            System.out.println("Não há funcionários no momento.");
+                        if (!resposta.equalsIgnoreCase("s")) {
                             break;
                         }
 
-                        consultar(funcionarios);
+                        sc.nextLine();
+                    }
 
+                    break;
+                case 2:
 
+                    if (funcionarios.isEmpty()) {
+                        System.out.println("Não há funcionários no momento.");
                         break;
-                    case 3:
+                    }
 
-                        consultaSalario(departamentos);
+                    consultar(funcionarios);
+
+
+                    break;
+                case 3:
+                    if (funcionarios.isEmpty()) {
+                        System.out.println("Não há departamentos cadastrados no sistema.");
                         break;
+                    }
+                    consultaSalario(departamentos);
+                    break;
 
-                    case 4:
+                case 4:
+
+                    if (funcionarios.isEmpty()) {
+                        System.out.println("Não há funcionários no momento.");
+                        break;
+                    }
+
+                    while (true) {
+
+                        atualizaSalario(sc);
+
+                        System.out.print("Deseja atualizar o salário de outro funcionário? (s/n): ");
+                        String resposta = sc.next();
+                        //novaTarefa.nextLine();
+
+                        if (!resposta.equalsIgnoreCase("s")) {
+                            break;
+                        }
+
+                        sc.nextLine();
+                    }
+                    break;
+
+                case 5:
+
+                    if (funcionarios.isEmpty()) {
+                        System.out.println("Não há funcionários no momento.");
+                        break;
+                    }
+
+                    while (true) {
+
+                        Realocar(sc);
+
+                        System.out.print("Deseja realocar outro funcionário? (s/n): ");
+                        String resposta = sc.next();
+                        //novaTarefa.nextLine();
+
+                        if (!resposta.equalsIgnoreCase("s")) {
+                            break;
+                        }
+
+                        sc.nextLine();
+                    }
+                    break;
+
+                case 6:
+
+                    if (funcionarios.isEmpty()) {
+                        System.out.println("Não há funcionários a serem removidos.");
+                        break;
+                    }
+
+                    while (true) {
+
                         remover(sc);
-                        break;
 
-                    case 5:
-                        System.out.println("Até mais! :)");
-                        break;
+                        if (funcionarios.isEmpty()) {
+                            System.out.println("Não há mais funcionários a serem removidos.");
+                            break;
+                        }
 
-                    default:
-                        System.out.println("Opção inválida. Por favor, digite outro número.");
+                        System.out.print("Deseja remover outro funcionário? (s/n): ");
+                        String resposta = sc.next();
+                        //novaTarefa.nextLine();
 
-                }
-            } while (n2 != 5);
+                        if (!resposta.equalsIgnoreCase("s")) {
+                            break;
+                        }
 
-        }
+                        sc.nextLine();
+                    }
+                    break;
+
+                case 7:
+                    System.out.println("Até mais! :)");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Por favor, digite outro número.");
+
+            }
+        } while (n2 != 7);
+
     }
+}
 
 
 
